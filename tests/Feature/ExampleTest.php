1daa22180b7e728c9bb20bcde1f207cdacb5f8b8
Eni <estefanielunardi@users.noreply.github.com>
2021-02-24 11:03:19 +0100

    test faild from another branch

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/faildAgain');
 
         $response->assertStatus(200);
     }
