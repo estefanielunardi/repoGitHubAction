1ee20f214ed14321d9a7a76da21aeb40b296c8be
Eni <estefanielunardi@users.noreply.github.com>
2021-03-11 22:59:18 +0100

    test past

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/faildAgain');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
